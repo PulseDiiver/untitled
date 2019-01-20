e73a5478121f7ff77ee410db8fe206a23cd8167a
PulseDiiver <44818214+PulseDiiver@users.noreply.github.com>
2019-01-21 01:51:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("wqeqwe!");
+        System.out.println("111111111111111111111");
     }
 }
